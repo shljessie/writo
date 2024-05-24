ba43aa25efcfaac3fd1def89758c3df5d3e625f2
shljessie <sl994@cornell.edu>
2024-05-23 21:10:39 -0700

    deploy check

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/writo/', // Replace 'writo' with the name of your repository
 })
